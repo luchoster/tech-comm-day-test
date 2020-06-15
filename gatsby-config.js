eb7be7415d5e34a721b2ed4811c70d23fcab1984
Lucho <luchoster@gmail.com>
2020-06-15 10:23:19 -0700

    getting the exhibitors

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,32 @@
+// In your gatsby-config.js file
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsy Started`,
     description: `Simple Gatsby theme with scss and material-ui support`,
-    author: `@luchoster`,
+    author: `@luchoster`
   },
   plugins: [
     {
-      resolve: 'gatsby-theme-luchoster-base',
+      resolve: 'gatsby-theme-luchoster-base'
+    },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: process.env.SANITY_ID,
+        dataset: process.env.SANITY_DATASET,
+        watchMode: false,
+        token: process.env.SANITY_TOKEN
+      }
     },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
+        pathToConfigModule: `src/utils/typography`
+      }
+    }
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 
-export default () => (
-  <Layout>
-    <SEO />
-    <div className="container">
-      <h1 className="text--center animated fadeIn">World!</h1>
-    </div>
-  </Layout>
-)
+export default props => {
+  const {
+    allSanityExhibitors: { edges: data }
+  } = props.data
+
+  return (
+    <Layout>
+      <SEO />
+      <div className="container">
+        {data.map(({ node: ex }, index) => {
+          return <h1 key={index}>{ex.title}</h1>
+        })}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allSanityExhibitors {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+  }
+`
